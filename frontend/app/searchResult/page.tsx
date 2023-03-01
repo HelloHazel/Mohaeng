31585a2ebd00b6844e8d58c9aea871681e71f617
saerom <sagesaeromkim@gmail.com>
2023-03-01 11:51:15 +0900

    Refactor: data fetching 함수 변경 [#10]

--- frontend/app/searchResult/page.tsx
+++ frontend/app/searchResult/page.tsx
@@ -3,36 +3,62 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-type KeywordData = {
+type Keyword = {
+  addr: string;
   id: string;
-  title: string;
   image: string;
+  mapx: string;
+  mapy: string;
+  tel: string;
+  title: string;
   review: number;
 };
 
+// useEffect(() => {
+//   const handler = async () => {
+//     const response = await fetch("/api/keyword", {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     const data = await response.json();
+//     console.log(data);
+//   };
+//   handler();
+// }, []);
 export default function SearchResult(): JSX.Element {
-  const places: KeywordData[] = require("/public/data/keyword.json");
+  const [keywordData, setKeywordData] = useState<Keyword[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("/api/keyword");
+      const data = await res.json();
+      setKeywordData(data);
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
       <section className={styles.section}>
         <h2 className={styles.h2}>검색하신 결과 </h2>
-        <ul className={styles.placeList}>
-          {places?.map((place) => (
-            <li className={styles.item} key={place.id}>
-              <Link className={styles.Link} href={`/place/${place.id}`}>
+        <ul className={styles.keywordList}>
+          {keywordData?.map((keyword) => (
+            <li className={styles.item} key={keyword.id}>
+              <Link className={styles.Link} href={`/place/${keyword.id}`}>
                 <Image
                   className={styles.img}
-                  src={place.image}
-                  alt={place.title}
+                  src={keyword.image}
+                  alt={keyword.title}
                   width={257}
                   height={233}
                 />
-                <span className={styles.textBlock}>
-                  <p className={styles.title}>{place.title}</p>
-                  <p className={styles.review}>{place.review}건의 리뷰</p>
+                <span className={styles.keywordInfo}>
+                  <p className={styles.title}>{keyword.title}</p>
+                  <p className={styles.review}>{keyword.review}건의 리뷰</p>
                 </span>
               </Link>
             </li>
